f5df92dcd930b0d49946a45965235bfbf1422fbb
JargeZ <lev@ph3.ru>
2024-04-06 22:22:12 +1100

    Decimal in tests, logs and better range

--- src/energy/tariffs/tests/test_total_calculations.py
+++ src/energy/tariffs/tests/test_total_calculations.py
@@ -1,15 +1,22 @@
 import datetime
+import logging
+from decimal import Decimal
 from zoneinfo import ZoneInfo
 
 from energy.tariffs.services.ConsumptionCalculator.Calculator import CalculatorService
 from energy.tests.BaseEnergyTest import BaseEnergyTest
 
+logger = logging.getLogger(__name__)
+
 
 class TestTotalCalculations(BaseEnergyTest):
-    def test_blue_supplier_set_total(self, blue_supplier, blue_customer):
-        EXPECTED_TOTAL_BLUE = 46.71 + 215.85 + 564.63 + 148.63 + 101.93 + 171.47 + 288.66 + 536.31
+    def test_blue_supplier_set_total(self, blue_supplier, blue_customer, caplog):
+        caplog.set_level(logging.INFO)
+
+        EXPECTED_TOTAL_BLUE = Decimal('46.71') + Decimal('215.85') + Decimal('564.63') + Decimal('148.63') + Decimal(
+            '101.93') + Decimal('171.47') + Decimal('288.66') + Decimal('536.31')
         FROM_DATE = datetime.datetime(2024, 1, 1, tzinfo=ZoneInfo("Australia/Sydney"))
-        TO_DATE = datetime.datetime(2024, 1, 31, tzinfo=ZoneInfo("Australia/Sydney")) + datetime.timedelta(days=1)
+        TO_DATE = datetime.datetime(2024, 2, 1, tzinfo=ZoneInfo("Australia/Sydney"))
 
         calculator = CalculatorService(
             supplier=blue_supplier,
@@ -18,4 +25,12 @@ class TestTotalCalculations(BaseEnergyTest):
             to_date=TO_DATE,
         )
 
-        assert calculator.calculate_total() == EXPECTED_TOTAL_BLUE
+        total = calculator.calculate_total()
+
+        for tariff, cost in calculator.total_by_tariff.items():
+            logger.info(f"{tariff.name}: {cost}")
+
+        delta = abs(total - EXPECTED_TOTAL_BLUE)
+        assert delta < Decimal('1')
+
+        assert round(total, 1) == round(EXPECTED_TOTAL_BLUE, 1)
