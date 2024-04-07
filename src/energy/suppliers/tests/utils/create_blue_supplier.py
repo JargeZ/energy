be2dacb73fff40e3769547dce2ab6b4b773fe533
JargeZ <lev@ph3.ru>
2024-04-07 16:55:31 +1000

    Change blue supplier gen

--- src/energy/suppliers/tests/utils/create_blue_supplier.py
+++ src/energy/suppliers/tests/utils/create_blue_supplier.py
@@ -1,12 +1,11 @@
-from datetime import date, time
+from datetime import date
 from decimal import Decimal
-from zoneinfo import ZoneInfo
 
 from energy.suppliers.models import EnergySupplier
 from energy.suppliers.models_factory import EnergySupplierFactory
-from energy.tariffs.models import TariffCondition, UnitType
+from energy.tariffs.models import ActivationRule, UnitType
 from energy.tariffs.models_factory import (
-    TariffConditionFactory,
+    ActivationRuleFactory,
     TariffFactory,
     TariffGroupFactory,
 )
@@ -27,38 +26,39 @@ def create_blue_supplier() -> EnergySupplier:
         unit_price=1.506849,
     )
     TariffFactory(
+        priority=1,
         supplier=supplier,
         group=energy_charges,
         name="Off-peak base energy charge",
         unit_type=UnitType.KWH.value,
         unit_price=0.063960,
         consumption_coefficient=LOSS_FACTOR,
-        condition=TariffConditionFactory(
-            name="Off-peak energy time",
-            type=TariffCondition.Type.TIME_RANGE.value,
-            time_from="09:00:00",
-            time_to="19:00:00",
-            inverted=True,
-        ),
+        activation_rules=[
+            # Base rule last by priority
+        ],
     )
     TariffFactory(
+        priority=2,
         supplier=supplier,
         group=energy_charges,
         name="Peak energy charge",
         unit_type=UnitType.KWH.value,
         unit_price=0.077990,
         consumption_coefficient=LOSS_FACTOR,
-        condition=TariffConditionFactory(
-            name="Peak energy time",
-            type=TariffCondition.Type.TIME_RANGE.value,
-            time_from="09:00:00",
-            time_to="19:00:00",
-        ),
+        activation_rules=[
+            ActivationRuleFactory(
+                name="Peak energy time",
+                parameter=ActivationRule.Parameter.TIME.value,
+                operator=ActivationRule.Operator.BETWEEN.value,
+                value=["09:00:00", "19:00:00"],
+            )
+        ],
     )
 
     network_charges = TariffGroupFactory(name="Network charges")
     # NETWORK CHARGES
     TariffFactory(
+        priority=0,
         supplier=supplier,
         group=network_charges,
         name="Daily network charge",
@@ -66,31 +66,29 @@ def create_blue_supplier() -> EnergySupplier:
         unit_price=3.288000,
     )
     TariffFactory(
+        priority=1,
         supplier=supplier,
         group=network_charges,
         name="Off-peak base network charge",
         unit_type=UnitType.KWH.value,
         unit_price=0.045300,
-        condition=TariffConditionFactory(
-            name="Off-peak network time",
-            type=TariffCondition.Type.TIME_RANGE.value,
-            time_from=time(15, 00, 00, tzinfo=ZoneInfo("Australia/Sydney")),
-            time_to=time(22, 00, 00, tzinfo=ZoneInfo("Australia/Sydney")),
-            inverted=True,
-        ),
+        activation_rules=[],
     )
     TariffFactory(
+        priority=2,
         supplier=supplier,
         group=network_charges,
         name="Peak network charge",
         unit_type=UnitType.KWH.value,
         unit_price=0.045300,
-        condition=TariffConditionFactory(
-            name="Peak network time",
-            type=TariffCondition.Type.TIME_RANGE.value,
-            time_from=time(15, 00, 00, tzinfo=ZoneInfo("Australia/Sydney")),
-            time_to=time(22, 00, 00, tzinfo=ZoneInfo("Australia/Sydney")),
-        ),
+        activation_rules=[
+            ActivationRuleFactory(
+                name="Peak energy time",
+                parameter=ActivationRule.Parameter.TIME.value,
+                operator=ActivationRule.Operator.BETWEEN.value,
+                value=["15:00:00", "22:00:00"],
+            )
+        ],
     )
 
     # METERS AND DEMAND
@@ -110,12 +108,14 @@ def create_blue_supplier() -> EnergySupplier:
         name="Summer demand charge",
         unit_type=UnitType.KW.value,
         unit_price=15.700000,
-        condition=TariffConditionFactory(
-            name="Summer demand time",
-            type=TariffCondition.Type.DATE_RANGE.value,
-            date_from=date(2023, 11, 30),
-            date_to=date(2024, 3, 1),
-        ),
+        activation_rules=[
+            ActivationRuleFactory(
+                name="Summer demand time",
+                parameter=ActivationRule.Parameter.DATE.value,
+                operator=ActivationRule.Operator.BETWEEN.value,
+                value=[date(2023, 11, 30), date(2024, 3, 1)],
+            )
+        ],
     )
 
     return supplier
