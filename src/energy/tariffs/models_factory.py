95b8c3a295b6b94804a615cbe3d9796c6ff273c2
JargeZ <lev@ph3.ru>
2024-04-07 16:56:48 +1000

    Test factory change

--- src/energy/tariffs/models_factory.py
+++ src/energy/tariffs/models_factory.py
@@ -1,16 +1,24 @@
+import factory
 from factory.django import DjangoModelFactory
 
-from energy.tariffs.models import Tariff, TariffCondition, TariffGroup
+from energy.tariffs.models import ActivationRule, Tariff, TariffGroup
 
 
-class TariffConditionFactory(DjangoModelFactory):
+class ActivationRuleFactory(DjangoModelFactory):
     class Meta:
-        model = TariffCondition
+        model = ActivationRule
 
 
 class TariffFactory(DjangoModelFactory):
     class Meta:
         model = Tariff
+
+    @factory.post_generation
+    def activation_rules(self, create, extracted, **kwargs):
+        if not create or not extracted:
+            return
+
+        self.activation_rules.add(*extracted)
 
 
 class TariffGroupFactory(DjangoModelFactory):
