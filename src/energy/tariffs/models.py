cc74b233b8cca02ccb0e851eb74273695f6786a9
JargeZ <lev@ph3.ru>
2024-04-07 16:27:33 +1000

    Change database model

--- src/energy/tariffs/models.py
+++ src/energy/tariffs/models.py
@@ -1,9 +1,12 @@
-from datetime import datetime
+from datetime import date, datetime, time
 from enum import Enum
+from typing import Annotated, Optional
 
+from annotated_types import Len
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-from django.db.models import CheckConstraint, Q
+from django_pydantic_field import SchemaField
+from pydantic import RootModel
 
 from energy.utils.enum_to_choises import EnumChoiceMixin
 
@@ -26,67 +29,37 @@ class UnitType(EnumChoiceMixin, Enum):
     FIXED = "fixed"
 
 
-class TariffCondition(models.Model):
-    class Type(EnumChoiceMixin, Enum):
-        TIME_RANGE = "time_range"
-        DATE_RANGE = "date_range"
-        WEEKDAY_RANGE = "weekday_range"
-        MIXED = "mixed"
+class ConditionValue(RootModel):
+    root: Optional[
+        Annotated[list[int], Len(min_length=2)]
+        | Annotated[list[date], Len(min_length=2)]
+        | Annotated[list[time], Len(min_length=2)]
+        | datetime
+        | int
+    ]
+
+
+class ActivationRule(models.Model):
+    class Parameter(EnumChoiceMixin):
+        DATE = "date"
+        TIME = "time"
+        WEEKDAY = "weekday"
+        CONSUMED = "consumed"
+
+    class Operator(EnumChoiceMixin, Enum):
+        EQ = "eq"
+        IN = "in"
+        NOT_IN = "not_in"
+        BETWEEN = "between"
+        NOT_BETWEEN = "not_between"
 
     name = models.CharField(max_length=255)
-    type = models.CharField(max_length=255, choices=Type.choices)
 
-    # TODO: possible more complex structure
-    inverted = models.BooleanField("Not in range", default=False)
+    parameter = models.CharField(max_length=255, choices=Parameter.choices)
+    operator = models.CharField(max_length=255, choices=Operator.choices)
 
-    # TODO: possible normalize by creating a separate model for each type
-    time_from = models.TimeField(null=True, blank=True)
-    time_to = models.TimeField(null=True, blank=True)
-
-    # NOTE: year may be redundant byt most likely no because of changing of tariffs every year or month
-    date_from = models.DateField(null=True, blank=True)
-    date_to = models.DateField(null=True, blank=True)
-
-    weekday_from = models.IntegerField(null=True, blank=True, validators=WEEKDAY_RANGE_VALIDATORS)
-    weekday_to = models.IntegerField(null=True, blank=True, validators=WEEKDAY_RANGE_VALIDATORS)
-
-    class Meta:
-        db_table = "tariff_condition"
-        constraints = [
-            CheckConstraint(
-                check=(
-                    Q(type="time_range") & Q(time_from__isnull=False, time_to__isnull=False)
-                    | Q(type="date_range") & Q(date_from__isnull=False, date_to__isnull=False)
-                    | Q(type="mixed")
-                    & Q(date_from__isnull=False, date_to__isnull=False, time_from__isnull=False, time_to__isnull=False)
-                    | Q(type="weekday_range") & Q(weekday_from__isnull=False, weekday_to__isnull=False)
-                ),
-                name="ranges_fields",
-            ),
-            CheckConstraint(
-                name="weekday_from_1_to_7_or_null",
-                check=(
-                    Q(weekday_from__gte=1, weekday_from__lte=7, weekday_to__gte=1, weekday_to__lte=7)
-                    | Q(weekday_from__isnull=True, weekday_to__isnull=True)
-                ),
-            ),
-        ]
-
-    def is_match(self, date: datetime) -> bool:
-        matches: list[bool] = []
-        if self.weekday_from and self.weekday_to:
-            matches.append(self.weekday_from <= date.isoweekday() <= self.weekday_to)
-
-        if self.date_from and self.date_to:
-            matches.append(self.date_from <= date.date() <= self.date_to)
-
-        if self.time_from and self.time_to:
-            matches.append(self.time_from <= date.time() <= self.time_to)
-
-        if self.type != TariffCondition.Type.MIXED:
-            assert len(matches) == 1
-
-        return not all(matches) if self.inverted else all(matches)
+    # TODO: can be normalized, extract list values in separate shareable model and so on
+    value: ConditionValue = SchemaField(null=True, blank=True)  # type: ignore[assignment]
 
 
 # Namespace for tariffs in which they should be uniq.
@@ -106,36 +79,17 @@ class Tariff(models.Model):
 
     unit_price = models.DecimalField(max_digits=15, decimal_places=10)
     unit_type = models.CharField(max_length=255, choices=UnitType.choices)
-    effective_after_consumption = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     priority = models.IntegerField(default=0)
     group = models.ForeignKey(TariffGroup, on_delete=models.CASCADE, null=False, blank=False)
     group_id: int
 
+    # not sure about it, just for deal with Loss factor at the moment
     consumption_coefficient = models.DecimalField(max_digits=10, decimal_places=5, default=1)
 
-    # NOTE: Possible many (but need to clarify AND/OR logic)
-    condition: TariffCondition | None = models.ForeignKey(
-        TariffCondition, on_delete=models.CASCADE, null=True, blank=True
-    )
+    activation_rules = models.ManyToManyField(ActivationRule, related_name="tariffs")
 
     class Meta:
         db_table = "tariff"
         unique_together = [
             ("priority", "group"),
-        ]
-        constraints = [
-            CheckConstraint(
-                name="effective_after_consumption_positive",
-                check=Q(effective_after_consumption__gte=0),
-            ),
-            CheckConstraint(
-                name="condition_set_for_kwh",
-                check=(
-                    Q(unit_type="kWh") & Q(condition__isnull=False)
-                    | Q(unit_type="KW") & Q(condition__isnull=False)
-                    | Q(unit_type="kWA") & Q(condition__isnull=False)
-                    | Q(unit_type="days") & Q(condition__isnull=True)
-                    | Q(unit_type="fixed") & Q(condition__isnull=True)
-                ),
-            ),
         ]
